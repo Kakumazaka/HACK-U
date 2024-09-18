daf9da340e99adcb7d724ac7f917437ff2dbfd05
momomo546 <108210454+momomo546@users.noreply.github.com>
2024-09-18 11:24:27 +0900

    showMemoの処理追加

--- aituber-kit/src/components/bottomButton.tsx
+++ aituber-kit/src/components/bottomButton.tsx
@@ -1,48 +1,93 @@
 import React, { useCallback, useRef, useState, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+
 import { IconButton } from './iconButton'
+import { Form } from './form';
+import { handleSendChatFn } from '@/features/chat/handlers';
 
 export const BottomButton = () => {
-    const showMemo = () => {
-        return
-    }
+    const [showForm, setShowForm] = useState(false);
+    const { t } = useTranslation()
+    const handleSendChat = handleSendChatFn({
+        NotConnectedToExternalAssistant: t('NotConnectedToExternalAssistant'),
+        APIKeyNotEntered: t('APIKeyNotEntered'),
+    })
+
+    //memoの表示と読み上げ
+    const showMemo = useCallback(() => {
+        if (!showForm) {
+            //ここに初期メッセージ
+            handleSendChat("おはよう");
+        }
+        setShowForm(true);
+    }, [showForm, handleSendChat])
+    //memo編集画面への遷移
     const editMemo = () => {
 
     }
+    //バーコードの読み取り
     const readBarCode = () => {
 
     }
+    const back = () => {
+        setShowForm(false);
+    }
     return (
         <>
-            <div className="absolute bottom-0 z-20 m-24" style={{
-                left: '50%',
-                transform: 'translate(-50%, 0%)',
-            }}>
-                <div
-                    className="grid grid-flow-col gap-[8px] mb-40"
-                    style={{ width: 'max-content' }}
-                >
-                    <div className="order-3">
-                        <IconButton
-                            iconName="32/CommentOn"
-                            isProcessing={false}
-                            onClick={showMemo}
-                        />
+            {showForm ? (
+                <><Form />
+                    <div className="absolute bottom-0 z-20 m-24" style={{
+                        right: '0px',
+                        bottom: '12px',
+                        transform: 'translate(0%, 0%)',
+                    }}>
+                        <div
+                            className="grid grid-flow-col gap-[8px] mb-40"
+                            style={{ width: 'max-content' }}
+                        >
+                            <div className="order-3">
+                                <IconButton
+                                    iconName="24/Prev"
+                                    isProcessing={false}
+                                    onClick={back}
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div className="order-4">
-                        <IconButton
-                            iconName="32/Edit"
-                            isProcessing={false}
-                            onClick={editMemo}
-                        />
-                    </div>
-                    <div className="order-5">
-                        <IconButton
-                            iconName="32/Camera"
-                            isProcessing={false}
-                            onClick={readBarCode}
-                        />
+                </>
+            ) : (
+                <div className="absolute bottom-0 z-20 m-24" style={{
+                    left: '50%',
+                    transform: 'translate(-50%, 0%)',
+                }}>
+                    <div
+                        className="grid grid-flow-col gap-[8px] mb-40"
+                        style={{ width: 'max-content' }}
+                    >
+                        <div className="order-3">
+                            <IconButton
+                                iconName="32/CommentOn"
+                                isProcessing={false}
+                                onClick={showMemo}
+                            />
+                        </div>
+                        <div className="order-4">
+                            <IconButton
+                                iconName="32/Edit"
+                                isProcessing={false}
+                                onClick={editMemo}
+                            />
+                        </div>
+                        <div className="order-5">
+                            <IconButton
+                                iconName="32/Camera"
+                                isProcessing={false}
+                                onClick={readBarCode}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>);
+            )}
+        </>
+    );
 };
